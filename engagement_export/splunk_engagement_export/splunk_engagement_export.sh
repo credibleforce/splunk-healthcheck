ec6aec08766bb39624059dd59747c6777cdf82e6
Scalatar <58786780+scalatar@users.noreply.github.com>
2020-08-24 20:49:36 -0700

    Update splunk_engagement_export.sh

--- engagement_export/splunk_engagement_export/splunk_engagement_export.sh
+++ engagement_export/splunk_engagement_export/splunk_engagement_export.sh
@@ -9,7 +9,7 @@ fi
 for s in $(grep -E "^\[.+]$" /opt/splunk/etc/apps/engagement_healthcheck/local/savedsearches.conf | sed 's/\[//g' | sed 's/\]//g'); do
 echo "-------------------------------"
 echo "Exporting: $s"
-/opt/splunk/bin/splunk search "| savedsearch $s" -app engagement_healthcheck -maxout 0 -preview 0 -output json -earliest_time -24h -latest_time now> engagement_data/$s.json
+/opt/splunk/bin/splunk search "| savedsearch $s" -app engagement_healthcheck -maxout 0 -preview 0 -output json -earliest_time -7d -latest_time now> engagement_data/$s.json
 echo "Complete."
 done
 echo "Creating archive of reports..."
